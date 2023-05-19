51cb15d53bfc1caacd0c74e70798c0a6d8288e4c
Matheus Hora <matheus11hora@gmail.com>
2023-05-19 11:59:42 -0300

    Termiando a tela de add task

--- to_do_app/lib/src/pages/add_taks_bar.dart
+++ to_do_app/lib/src/pages/add_taks_bar.dart
@@ -5,6 +5,7 @@ import 'package:get/get.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
 import 'package:to_do_app/src/pages/widget/custom_inputs.dart';
+import 'package:to_do_app/src/shared/theme/others_color.g.dart';
 
 class AddTaskPage extends StatefulWidget {
   const AddTaskPage({super.key});
@@ -17,12 +18,21 @@ class _AddTaskPageState extends State<AddTaskPage> {
   DateTime _selectedDate = DateTime.now();
   String _endTime = "9:30 AM";
   String _startTime = DateFormat("hh:mm a").format(DateTime.now()).toString();
+
   int _selectedRemind = 5;
   List<int> remindList = [
     5,
     10,
     15,
     20,
+  ];
+
+  String _selectedRepet = "Nenhuma";
+  List<String> repetList = [
+    "Nenhuma",
+    "Diária",
+    "Semanal",
+    "Mensal",
   ];
 
   String getData(DateTime date) {
@@ -100,6 +110,97 @@ class _AddTaskPageState extends State<AddTaskPage> {
                     ),
                   ),
                 ],
+              ),
+              CustomInputs(
+                title: 'Lembrar',
+                hint: '$_selectedRemind minutos',
+                widget: DropdownButton(
+                  icon: const Icon(Icons.keyboard_arrow_down),
+                  iconSize: 32,
+                  elevation: 4,
+                  underline: Container(
+                    height: 0,
+                  ),
+                  items: remindList.map<DropdownMenuItem<String>>((int value) {
+                    return DropdownMenuItem<String>(
+                      value: value.toString(),
+                      child: Text(value.toString()),
+                    );
+                  }).toList(),
+                  onChanged: (String? value) {
+                    setState(() {
+                      _selectedRemind = int.parse(value!);
+                    });
+                  },
+                ),
+              ),
+              CustomInputs(
+                title: 'Repetir',
+                hint: '$_selectedRepet',
+                widget: DropdownButton(
+                  icon: const Icon(Icons.keyboard_arrow_down),
+                  iconSize: 32,
+                  elevation: 4,
+                  underline: Container(
+                    height: 0,
+                  ),
+                  items:
+                      repetList.map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(
+                        value,
+                      ),
+                    );
+                  }).toList(),
+                  onChanged: (String? value) {
+                    setState(() {
+                      _selectedRepet = value!;
+                    });
+                  },
+                ),
+              ),
+              const SizedBox(
+                height: 18,
+              ),
+              Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Etiquetas',
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      Wrap(
+                        children: List<Widget>.generate(
+                          3,
+                          (int index) {
+                            return Padding(
+                              padding: const EdgeInsets.only(right: 8),
+                              child: GestureDetector(
+                                onTap: (){
+                                  
+                                },
+                                child: CircleAvatar(
+                                  radius: 14,
+                                  backgroundColor: index == 0
+                                      ? OthersColors.bluishClr
+                                      : index == 1
+                                          ? OthersColors.pinkClr
+                                          : OthersColors.yellowClr,
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      )
+                    ],
+                  )
+                ],
               )
             ],
           ),
@@ -149,15 +250,17 @@ class _AddTaskPageState extends State<AddTaskPage> {
   }
 
   getTimeFromUser({required bool isStartTime}) async {
-    var pickedTime = _showTimePicker();
+    var pickedTime = await _showTimePicker();
+    String _formatedTime = pickedTime.format(context);
+
     if (pickedTime == null) {
     } else if (isStartTime == true) {
       setState(() {
-        _startTime = pickedTime;
+        _startTime = _formatedTime;
       });
     } else if (isStartTime == false) {
       setState(() {
-        _endTime = pickedTime;
+        _endTime = _formatedTime;
       });
     }
   }
